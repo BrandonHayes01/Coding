d6faf6b802a4d46637766933f7a5b7bbe0014131
BrandonHayes01 <55750198+BrandonHayes01@users.noreply.github.com>
2019-09-24 14:37:11 +0100

    Add files via upload

---
+++ James.java
@@ -0,0 +1,14 @@
+public class James {
+  public James() {
+    System.out.println("I have created a new James");
+  }
+
+  public void Brandon() {
+    System.out.println("Brandon is here");
+  }
+
+  public void Question() {
+    var x = input("Type in something to be repeated. ");
+    System.out.println(x);
+  }
+}
---
+++ Main.java
@@ -0,0 +1,19 @@
+class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    Pepega();
+    test();
+    James newvar = new James();
+    newvar.Brandon();
+    System.out.println("");
+    newvar.Question();
+  }
+  public static void Pepega() {
+    System.out.println("Yeet");
+  }
+
+  public static void test() {
+    System.out.println("Test");
+  }
+}
